e412fa20aa0ec78bb17dfd9a0202d3cfc9dcba12
choihana <chhana0703@gmail.com>
2023-05-12 15:59:22 +0900

    init

---
+++ src/main/java/com/example/hanghaegg/HanghaeGgApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghaegg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeGgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeGgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hanghaegg/HanghaeGgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanghaegg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeGgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
