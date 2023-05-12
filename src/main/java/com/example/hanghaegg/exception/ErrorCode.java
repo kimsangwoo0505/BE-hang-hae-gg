5d2ccd15af71c1a3c48844ac681ca8844d3ed7df
kimgun95 <harrison1995@naver.com>
2023-05-12 20:26:06 +0900

    feat: jwt exception을 위한 ErrorResponse 추가

---
+++ src/main/java/com/example/hanghaegg/exception/ErrorCode.java
@@ -0,0 +1,10 @@
+package com.example.hanghaegg.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+	String name();
+	HttpStatus getHttpStatus();
+	String getMessage();
+}
