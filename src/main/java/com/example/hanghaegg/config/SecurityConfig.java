66aa323dc47ea5f307e56937becb7b2e8899be21
kimgun95 <harrison1995@naver.com>
2023-05-15 20:13:53 +0900

    feat: 시큐리티 홈 경로 permitAll

--- src/main/java/com/example/hanghaegg/config/SecurityConfig.java
+++ src/main/java/com/example/hanghaegg/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
 			.requestMatchers("/swagger-ui/**").permitAll()
 			.requestMatchers("/v3/api-docs/**").permitAll()
 			.requestMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
-			.requestMatchers("/signup").permitAll() // 회원가입 접근 가능
+			.requestMatchers("/signup", "/").permitAll() // 회원가입 접근 가능
 			.anyRequest().authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
 			.and()
 			//== 소셜 로그인 설정 ==//
