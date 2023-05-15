57434b2568af07f0b9e719a4807653e65d897b1c
SangWon7242 <ivv11@naver.com>
2023-05-15 17:58:47 +0900

    fix:페이징처리,아이콘추가

--- src/main/java/com/example/hanghaegg/search/dto/InfoResponseDto.java
+++ src/main/java/com/example/hanghaegg/search/dto/InfoResponseDto.java
@@ -13,6 +13,7 @@ public class InfoResponseDto {
 	private	String rank;
 	private	String summonerName;
 	private	int leaguePoints;
+	private	String summonerIconUrl;
 	private	int wins;
 	private	int losses;
 	private List<MatchResponseDto> matchResults;
