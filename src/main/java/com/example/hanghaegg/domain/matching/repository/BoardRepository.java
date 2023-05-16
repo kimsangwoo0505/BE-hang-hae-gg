c51fc5be593d70939c2cd2fd3942e1f23d70d20b
kes <kmskes1125@gmail.com>
2023-05-17 01:26:11 +0900

    Chore: 안 쓰는 import 제거

--- src/main/java/com/example/hanghaegg/domain/matching/repository/BoardRepository.java
+++ src/main/java/com/example/hanghaegg/domain/matching/repository/BoardRepository.java
@@ -1,7 +1,5 @@
 package com.example.hanghaegg.domain.matching.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hanghaegg.domain.matching.entity.Board;
